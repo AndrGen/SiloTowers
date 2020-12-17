0dfea8a33305cefea86ec644b0ee5deb472c8ed6
AndrGen <31141640+AndrGen@users.noreply.github.com>
2020-12-17 22:00:16 +0300

    md5 тест

---
+++ SiloTowersSolution/src/Services/Silo/test/SiloTowersUnitTest/Md5Test.cs
@@ -0,0 +1,21 @@
+﻿using Common.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SiloTowersUnitTest
+{
+
+    [TestClass]
+    public class Md5Test
+    {
+
+        [TestMethod]
+        public void GenerateToken()
+        {
+            string str = "Это пароль";
+            var res = Md5Helper.CreateMD5(str);
+            Assert.AreEqual("C7CBB97BB3A7B44CD37922FB2D03D566", res, "Md5 хеш не совпал");
+        }
+
+       
+    }
+}
