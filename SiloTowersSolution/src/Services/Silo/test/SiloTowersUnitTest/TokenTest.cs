279fe6ec43f262fd125027896538ccc962f9974d
AndrGen <31141640+AndrGen@users.noreply.github.com>
2020-12-17 19:31:38 +0300

    добавлен юнит тест

---
+++ SiloTowersSolution/src/Services/Silo/test/SiloTowersUnitTest/TokenTest.cs
@@ -0,0 +1,21 @@
+﻿using Common.Helper;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SiloTower.Api.Implementations;
+using SiloTower.Interfaces.Auth;
+
+namespace SiloTowersUnitTest
+{
+
+    [TestClass]
+    public class TokenTest
+    {
+        [TestMethod]
+        public void GenerateToken()
+        {
+            var config = AppFileConfiguration.GetConfiguration(@"appsettings.json");
+            IToken tokenImpl = new TokenImpl(config);
+            string token = tokenImpl.GenerateToken();
+            Assert.IsNotNull(token, "Токен не задан");
+        }
+    }
+}
